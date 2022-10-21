46d046ec9474abef4ea9e8defdf86df73161e1c8
Dima Golik <1dimagolik1@mail.ru>
2022-10-21 14:13:30 +0700

    project creation

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 50_001;
+        int x = 20 * 1;
+        int bonus = ticket / x;
+        System.out.println(bonus);
+    }
+}
